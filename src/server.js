2a0e7d820a1c26867d0c5dce68c8f098cd24d5e9
pedro henrique martins paiva <pedrofh3nix@gmail.com>
2021-06-06 20:25:50 -0300

    add a cord dependency

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 
 const dotenv = require('dotenv')
 
@@ -7,6 +8,7 @@ require('./database')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
